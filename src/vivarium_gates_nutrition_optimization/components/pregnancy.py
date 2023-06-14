d41c969941fe287c156f4c916298d3bcff204bfb
Patrick Nast <pnast@uw.edu>
2023-06-14 13:12:07 -0700

    linting

--- src/vivarium_gates_nutrition_optimization/components/pregnancy.py
+++ src/vivarium_gates_nutrition_optimization/components/pregnancy.py
@@ -1,10 +1,21 @@
 from vivarium_public_health.disease import DiseaseModel, DiseaseState, SusceptibleState
+
 from vivarium_gates_nutrition_optimization.constants import data_keys, models
+
 
 def Pregnancy():
     not_pregnant = SusceptibleState(models.PREGNANCY_MODEL_NAME)
-    pregnant = DiseaseState(models.PREGNANCY_MODEL_NAME,get_data_functions={"prevalence": lambda *_: 1.0,
-                                                                             "disability_weight": lambda *_: 0.0,
-                                                                             "excess_mortality_rate":lambda *_: 0.0 })
+    pregnant = DiseaseState(
+        models.PREGNANCY_MODEL_NAME,
+        get_data_functions={
+            "prevalence": lambda *_: 1.0,
+            "disability_weight": lambda *_: 0.0,
+            "excess_mortality_rate": lambda *_: 0.0,
+        },
+    )
     pregnant.allow_self_transitions()
-    return DiseaseModel(models.PREGNANCY_MODEL_NAME,states=[not_pregnant, pregnant],get_data_functions={"cause_specific_mortality_rate":lambda *_: 0.0})
+    return DiseaseModel(
+        models.PREGNANCY_MODEL_NAME,
+        states=[not_pregnant, pregnant],
+        get_data_functions={"cause_specific_mortality_rate": lambda *_: 0.0},
+    )
