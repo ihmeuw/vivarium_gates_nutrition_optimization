3ea15256b4069826c34102da805c51afdf15aab7
Patrick Nast <pnast@uw.edu>
2023-06-14 18:08:40 -0700

    Subclass SusceptibleState to get not pregnant

--- src/vivarium_gates_nutrition_optimization/components/pregnancy.py
+++ src/vivarium_gates_nutrition_optimization/components/pregnancy.py
@@ -2,9 +2,12 @@ from vivarium_public_health.disease import DiseaseModel, DiseaseState, Susceptib
 
 from vivarium_gates_nutrition_optimization.constants import data_keys, models
 
+class NotPregnantState(SusceptibleState):
+    def __init__(self, cause, *args, **kwargs):
+        super(SusceptibleState, self).__init__(cause, *args, name_prefix="not_", **kwargs)
 
 def Pregnancy():
-    not_pregnant = SusceptibleState(models.PREGNANCY_STATE)
+    not_pregnant = NotPregnantState(models.PREGNANCY_STATE)
     pregnant = DiseaseState(
         models.PREGNANCY_STATE,
         get_data_functions={
