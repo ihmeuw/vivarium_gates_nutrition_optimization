cbf01c1c75fbaa7ccaae745d5e9d519788def12b
Patrick Nast <pnast@uw.edu>
2023-06-26 11:20:09 -0700

    address nits

--- src/vivarium_gates_nutrition_optimization/constants/data_values.py
+++ src/vivarium_gates_nutrition_optimization/constants/data_values.py
@@ -5,9 +5,9 @@ from typing import NamedTuple
 # Disease Model Parameters #
 ############################
 class _Durations(NamedTuple):
-     ## Days
+    ## Days
     FULL_TERM = 40 * 7
-    POSTPARTUM_DURATION = 6 * 7
+    POSTPARTUM = 6 * 7
     DETECTION = 6 * 7
     PARTIAL_TERM = 24 * 7
 
