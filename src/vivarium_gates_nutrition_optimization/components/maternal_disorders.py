51682d00d993fdbd7a8b71deb6fe088caaef8c8e
Patrick Nast <pnast@uw.edu>
2023-07-14 13:58:42 -0700

    add to models.py

--- src/vivarium_gates_nutrition_optimization/components/maternal_disorders.py
+++ src/vivarium_gates_nutrition_optimization/components/maternal_disorders.py
@@ -1,26 +1,33 @@
-import pandas as pd
-from vivarium.framework.engine import Builder
-from vivarium.framework.population import SimulantData
-from vivarium.framework.values import Pipeline
 from vivarium_public_health.disease import DiseaseModel, SusceptibleState, RecoveredState
 
 from vivarium_gates_nutrition_optimization.components.disease import DiseaseState
 from vivarium_gates_nutrition_optimization.constants import data_keys, models
 from vivarium_gates_nutrition_optimization.constants.data_values import DURATIONS
-from vivarium_gates_nutrition_optimization.constants.metadata import (
-    ARTIFACT_INDEX_COLUMNS,
-)
+
 
 def MaternalDisorders():
     cause = models.MATERNAL_DISORDERS_STATE_NAME
     healthy = SusceptibleState(cause)
-    infected = DiseaseState(cause)
+    infected = DiseaseState(
+        cause,
+        get_data_functions={
+            "prevalence": lambda *_: 0.0,
+            "disability_weight": lambda *_: 0.0,  ## Add proper DW here--get through artifact?
+            "excess_mortality_rate": lambda *_: 0.0,  ## add proper EMR here -- get through artifact?
+            "dwell_time": lambda *_: DURATIONS.MATERNAL_DISORDERS,
+        },
+    )
     recovered = RecoveredState(cause)
 
     healthy.allow_self_transitions()
-    healthy.add_transition(infected, source_data_type="rate")
+    healthy.add_transition(
+        infected, source_data_type="rate"
+    )  ## Should this still be proportion?
     infected.allow_self_transitions()
-    infected.add_transition(recovered, source_data_type="rate")
+    infected.add_transition(
+        recovered, source_data_type="rate"
+    )  ## Should this still be proportion?
     recovered.allow_self_transitions()
 
     return DiseaseModel(cause, states=[healthy, infected, recovered])
+    ## Add CSMR here--get through artifact?
