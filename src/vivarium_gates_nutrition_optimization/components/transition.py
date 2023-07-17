8ce55f6808ce69babce93fcfaa49044b8286689d
Patrick Nast <pnast@uw.edu>
2023-07-17 14:26:25 -0700

    skip maternal disorder postprocessing

--- src/vivarium_gates_nutrition_optimization/components/transition.py
+++ src/vivarium_gates_nutrition_optimization/components/transition.py
@@ -1,4 +1,5 @@
 from vivarium_public_health.disease.transition import RateTransition
+from vivarium.framework.utilities import rate_to_probability
 import pandas as pd
 
 class ParturitionSelectionRateTransition(RateTransition):
@@ -9,3 +10,7 @@ class ParturitionSelectionRateTransition(RateTransition):
         joint_paf = self.joint_paf(sub_pop)
         transition_rate.loc[sub_pop] = base_rates * (1 - joint_paf)
         return transition_rate
+    
+    ## Skip Post-Processing so we don't need to rescale by timestep
+    def _probability(self, index):
+        return rate_to_probability(self.transition_rate(index,skip_post_processor=True))
