97d2968c2da8eaed1fc5675317470cf82d10db30
Patrick Nast <pnast@uw.edu>
2023-07-17 18:42:30 -0700

    add a comment

--- src/vivarium_gates_nutrition_optimization/components/mortality.py
+++ src/vivarium_gates_nutrition_optimization/components/mortality.py
@@ -5,6 +5,8 @@ from vivarium_public_health.population.mortality import Mortality as Mortality_
 
 class Mortality(Mortality_):
     def _get_mortality_rate(self, builder: Builder) -> Pipeline:
+        ## We want to register a value producer instead of a rate producer, so the
+        ## Rescaling doesn't happen.
         return builder.value.register_value_producer(
             self.mortality_rate_pipeline_name,
             source=self._calculate_mortality_rate,
