500ce4ca75d672c41ce158eb4c508adae2aec85c
Patrick Nast <pnast@uw.edu>
2023-07-24 12:32:45 -0700

    Revert "Merge branch 'feature/pnast/MIC-4261-Model-2.1-Implementation' into feature/pnast/Model-2.1-Post-Processing"
    This reverts commit f136c97f6ec77cd65e1275e4f06ccf26b0f011f2, reversing
    changes made to 5f749394a3b3616bf9b5e2b2a7104e79bd4e4f6b.

--- src/vivarium_gates_nutrition_optimization/components/disease.py
+++ src/vivarium_gates_nutrition_optimization/components/disease.py
@@ -420,7 +420,7 @@ class ParturitionSelectionState(SusceptibleState):
             output,
             get_data_functions={
                 "proportion": lambda builder, cause: builder.data.load(
-                    f"cause.{cause}.incident_probability"
+                    data_keys.MATERNAL_DISORDERS.INCIDENT_PROBABILITY
                 )
             },
             **kwargs,
