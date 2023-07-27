cc43756c50331f51a46c7c6071495ea9dea5007c
Patrick Nast <pnast@uw.edu>
2023-07-27 13:41:28 -0700

    add data keys back

--- src/vivarium_gates_nutrition_optimization/constants/data_keys.py
+++ src/vivarium_gates_nutrition_optimization/constants/data_keys.py
@@ -102,10 +102,28 @@ class __MaternalHemorrhage(NamedTuple):
 MATERNAL_HEMORRHAGE = __MaternalHemorrhage()
 
 
+class _Hemoglobin(NamedTuple):
+    MEAN: TargetString = TargetString("risk_factor.hemoglobin.mean")
+    STANDARD_DEVIATION: TargetString = TargetString(
+        "risk_factor.hemoglobin.standard_deviation"
+    )
+
+    @property
+    def name(self):
+        return "hemoglobin"
+
+    @property
+    def log_name(self):
+        return "hemoglobin"
+
+
+HEMOGLOBIN = _Hemoglobin()
+
 MAKE_ARTIFACT_KEY_GROUPS = [
     POPULATION,
     PREGNANCY,
     LBWSG,
     MATERNAL_DISORDERS,
     MATERNAL_HEMORRHAGE,
+    HEMOGLOBIN,
 ]
