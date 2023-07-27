b3bc6cd5b7b5ba8b02ccf15c50d2a50830671027
Patrick Nast <pnast@uw.edu>
2023-07-27 16:40:37 -0700

    add anemia even more

--- src/vivarium_gates_nutrition_optimization/components/__init__.py
+++ src/vivarium_gates_nutrition_optimization/components/__init__.py
@@ -1,5 +1,5 @@
 from .children import BirthRecorder
-from .hemoglobin import Hemoglobin
+from .hemoglobin import Hemoglobin, Anemia
 from .maternal_disorders import MaternalDisorders, MaternalHemorrhage
 from .mortality import MaternalMortality
 from .observers import MaternalMortalityObserver, PregnancyObserver, ResultsStratifier
