33977d21b5f2dbd799350f2e40a3b804e5f9e1a9
Patrick Nast <pnast@uw.edu>
2023-07-28 08:25:41 -0700

    comments

--- src/vivarium_gates_nutrition_optimization/components/hemoglobin.py
+++ src/vivarium_gates_nutrition_optimization/components/hemoglobin.py
@@ -258,14 +258,14 @@ class Anemia:
     def compute_disability_weight(self, index: pd.Index):
         anemia_levels = self.anemia_levels(index)
         raw_anemia_disability_weight = anemia_levels.map(ANEMIA_DISABILITY_WEIGHTS)
-        ## Why?
+        ## rescale postpartum YLDs to account for the pause
         postpartum_scalar = (
             data_values.DURATIONS.POSTPARTUM + data_values.DURATIONS.PARTURITION
         ) / data_values.DURATIONS.POSTPARTUM
         dw_map = {
             models.NOT_PREGNANT_STATE_NAME: raw_anemia_disability_weight,
             models.PREGNANT_STATE_NAME: raw_anemia_disability_weight,
-            ## I need to clarify exactly how and why we are pausing YLD accumulation
+            ## Pause YLD accumulation during the parturition state
             models.PARTURITION_STATE_NAME: pd.Series(0, index=index),
             models.POSTPARTUM_STATE_NAME: postpartum_scalar * raw_anemia_disability_weight,
         }
