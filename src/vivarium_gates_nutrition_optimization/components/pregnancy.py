8983827336a9ebff3239c8350982c6ce2fd724eb
Patrick Nast <pnast@uw.edu>
2023-08-01 16:42:29 -0700

    adjust model

--- src/vivarium_gates_nutrition_optimization/components/pregnancy.py
+++ src/vivarium_gates_nutrition_optimization/components/pregnancy.py
@@ -96,7 +96,7 @@ class PregnantState(DiseaseState):
         )
         child_status["pregnancy_duration"] = pd.to_timedelta(
             # (low + (high - low) * draw), unit="days"
-            7 * 1,
+            7 * 2,
             unit="days",
         )
         return child_status
@@ -105,7 +105,7 @@ class PregnantState(DiseaseState):
         child_status = self.new_children.generate_children(full_term_pop)
         child_status["pregnancy_duration"] = pd.to_timedelta(
             # 7 * child_status["gestational_age"], unit="days"
-            7 * 1,
+            7 * 2,
             unit="days",
         )
         return child_status.drop(columns=["gestational_age"])
