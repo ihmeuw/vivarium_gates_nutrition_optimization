cfb19a6a0e786108262c6e4f540b21f044dbb35b
Patrick Nast <pnast@uw.edu>
2023-08-17 15:53:25 -0700

    add background morbidity

--- src/vivarium_gates_nutrition_optimization/components/__init__.py
+++ src/vivarium_gates_nutrition_optimization/components/__init__.py
@@ -3,6 +3,7 @@ from .hemoglobin import Anemia, Hemoglobin
 from .intervention import MaternalInterventions
 from .maternal_bmi import MaternalBMIExposure
 from .maternal_disorders import MaternalDisorders, MaternalHemorrhage
+from .morbidity import BackgroundMorbidity
 from .mortality import MaternalMortality
 from .observers import (
     AnemiaObserver,
