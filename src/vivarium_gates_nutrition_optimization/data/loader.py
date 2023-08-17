6f0f7431102374472db9251810e7586cf9ee08a4
patricktnast <130876799+patricktnast@users.noreply.github.com>
2023-08-17 10:04:52 -0700

    Model 7.0 implementation (#53)
    * add adjustment to birth outcome probabilities
    
    * add draw-level sampling of MMS and BEP RRs
    
    * comment
    
    * adjust stillbirth probability
    
    * lint
    
    * fix bugs
    
    * adjust modifiers
    
    * add to setup
    
    * lint
    
    * use more appropriate dependency

--- src/vivarium_gates_nutrition_optimization/data/loader.py
+++ src/vivarium_gates_nutrition_optimization/data/loader.py
@@ -452,11 +452,12 @@ def load_ifa_coverage(key: str, location: str) -> pd.DataFrame:
     df = df.drop(columns=["location_id", "location_name"]).set_index(["draw"]).T
     return df
 
+
 def load_supplementation_stillbirth_rr(key: str, location: str) -> pd.DataFrame:
     try:
         distribution = data_values.INTERVENTION_STILLBIRTH_RRS[key]
     except KeyError:
-        raise ValueError(f'Unrecognized key {key}')
+        raise ValueError(f"Unrecognized key {key}")
 
     dist = sampling.get_lognorm_from_quantiles(*distribution)
     # Don't hash on key because we want simulants to have the same percentile
@@ -468,6 +469,25 @@ def load_supplementation_stillbirth_rr(key: str, location: str) -> pd.DataFrame:
         index=["relative_risk"],
     )
     return stillbirth_rr
+
+
+def load_supplementation_stillbirth_rr(key: str, location: str) -> pd.DataFrame:
+    try:
+        distribution = data_values.INTERVENTION_STILLBIRTH_RRS[key]
+    except KeyError:
+        raise ValueError(f"Unrecognized key {key}")
+
+    dist = sampling.get_lognorm_from_quantiles(*distribution)
+    # Don't hash on key because we want simulants to have the same percentile
+    # for MMS RR as for BEP
+    rng = np.random.default_rng(get_hash(f"stillbirth_rr_{location}"))
+    stillbirth_rr = pd.DataFrame(
+        [dist.rvs(size=1000, random_state=rng)],
+        columns=vi_globals.DRAW_COLUMNS,
+        index=["relative_risk"],
+    )
+    return stillbirth_rr
+
 
 ##############
 #   Helpers  #
