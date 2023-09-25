35b24c07a83bf083f75be396d73523e5e24281c7
Patrick Nast <pnast@uw.edu>
2023-09-25 13:28:09 -0700

    refactor the exclusionstates

--- src/vivarium_gates_nutrition_optimization/components/disease.py
+++ src/vivarium_gates_nutrition_optimization/components/disease.py
@@ -33,18 +33,21 @@ class ParturitionSelectionState(SusceptibleState):
 
 
 class ParturitionSelectionTransition(ProportionTransition):
-    def setup(self, builder: Builder):
-        super().setup(builder)
 
-        pipeline_name = f"{self.output_state.cause}.transition_proportion"
+    @property
+    def columns_required(self) -> List[str]:
+        return ["alive", "pregnancy"]
+
+    def setup(self, builder: Builder) -> None:
+        super().setup(builder)
+        pipeline_name = f"{self.output_state.state_id}.transition_proportion"
         self.proportion_pipeline = builder.value.register_value_producer(
             pipeline_name,
             source=self.compute_transition_proportion,
             requires_columns=["age", "sex", "alive"],
         )
-        self.population_view = builder.population.get_view(["alive", "pregnancy"])
 
-    def compute_transition_proportion(self, index):
+    def compute_transition_proportion(self, index) -> pd.Series:
         transition_proportion = pd.Series(0.0, index=index)
         sub_pop = self.population_view.get(
             index, query="(alive == 'alive') & (pregnancy == 'parturition')"
@@ -53,24 +56,86 @@ class ParturitionSelectionTransition(ProportionTransition):
         transition_proportion.loc[sub_pop] = self.proportion(sub_pop)
         return transition_proportion
 
-    def _probability(self, index):
+    def _probability(self, index) -> pd.Series:
         return self.proportion_pipeline(index)
 
 
 class ParturitionExclusionState(DiseaseState):
-    def setup(self, builder: Builder):
-        super().setup(builder)
-        view_columns = self.columns_created + [self._model, "alive", "pregnancy", "tracked"]
-        self.population_view = builder.population.get_view(view_columns)
 
-    def get_disability_weight_pipeline(self, builder: Builder) -> Pipeline:
-        return builder.value.register_value_producer(
-            f"{self.state_id}.disability_weight",
-            source=self.compute_disability_weight,
-            requires_columns=["age", "sex", "alive", "pregnancy", self._model],
+    @property
+    def columns_required(self) -> List[str]:
+        super().columns_required + ["pregnancy", "tracked"]
+    
+    def setup(self, builder: Builder) -> None:
+        def setup(self, builder: Builder) -> None:
+        """Performs this component's simulation setup.
+
+        Parameters
+        ----------
+        builder : `engine.Builder`
+            Interface to several simulation tools.
+        """
+        super(DiseaseState, self).setup(builder)
+        self.clock = builder.time.clock()
+
+        prevalence_data = self.load_prevalence_data(builder)
+        self.prevalence = builder.lookup.build_table(
+            prevalence_data, key_columns=["sex"], parameter_columns=["age", "year"]
         )
 
-    def compute_disability_weight(self, index: pd.Index):
+        birth_prevalence_data = self.load_birth_prevalence_data(builder)
+        self.birth_prevalence = builder.lookup.build_table(
+            birth_prevalence_data, key_columns=["sex"], parameter_columns=["year"]
+        )
+
+        dwell_time_data = self.load_dwell_time_data(builder)
+        self.dwell_time = builder.value.register_value_producer(
+            f"{self.state_id}.dwell_time",
+            source=builder.lookup.build_table(
+                dwell_time_data, key_columns=["sex"], parameter_columns=["age", "year"]
+            ),
+            requires_columns=["age", "sex"],
+        )
+
+        disability_weight_data = self.load_disability_weight_data(builder)
+        self.has_disability = is_non_zero(disability_weight_data)
+        self.base_disability_weight = builder.lookup.build_table(
+            disability_weight_data, key_columns=["sex"], parameter_columns=["age", "year"]
+        )
+        self.disability_weight = builder.value.register_value_producer(
+            f"{self.state_id}.disability_weight",
+            source=self.compute_disability_weight,
+            requires_columns=["age", "sex", "alive", self.model, "pregnancy"],
+        )
+        builder.value.register_value_modifier(
+            "disability_weight", modifier=self.disability_weight
+        )
+
+        excess_mortality_data = self.load_excess_mortality_rate_data(builder)
+        self.has_excess_mortality = is_non_zero(excess_mortality_data)
+        self.base_excess_mortality_rate = builder.lookup.build_table(
+            excess_mortality_data, key_columns=["sex"], parameter_columns=["age", "year"]
+        )
+        self.excess_mortality_rate = builder.value.register_rate_producer(
+            self.excess_mortality_rate_pipeline_name,
+            source=self.compute_excess_mortality_rate,
+            requires_columns=["age", "sex", "alive", self.model],
+            requires_values=[self.excess_mortality_rate_paf_pipeline_name],
+        )
+        paf = builder.lookup.build_table(0)
+        self.joint_paf = builder.value.register_value_producer(
+            self.excess_mortality_rate_paf_pipeline_name,
+            source=lambda idx: [paf(idx)],
+            preferred_combiner=list_combiner,
+            preferred_post_processor=union_post_processor,
+        )
+        builder.value.register_value_modifier(
+            "mortality_rate",
+            modifier=self.adjust_mortality_rate,
+            requires_values=[self.excess_mortality_rate_pipeline_name],
+        )
+
+    def compute_disability_weight(self, index: pd.Index) -> pd.Series:
         disability_weight = raw_disability_weight = pd.Series(0, index=index)
         with_condition = self.with_condition(index)
         raw_disability_weight.loc[with_condition] = self.base_disability_weight(
