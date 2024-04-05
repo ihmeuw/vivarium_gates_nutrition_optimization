d7e6e961564ee40f6fb4f9548fa4da233452be0d
albrja <albrja@uw.edu>
2024-04-05 12:04:47 -0700

    Remove import

--- src/vivarium_gates_nutrition_optimization/constants/data_values.py
+++ src/vivarium_gates_nutrition_optimization/constants/data_values.py
@@ -2,7 +2,6 @@ from typing import NamedTuple
 
 import numpy as np
 import pandas as pd
-from vivarium_inputs import globals as vi_globals
 
 from vivarium_gates_nutrition_optimization.constants import data_keys, models, paths
 
