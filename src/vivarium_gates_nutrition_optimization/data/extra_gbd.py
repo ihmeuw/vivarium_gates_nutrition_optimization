66a874f28bc3cafac6ce2789e95c114a80bd9fe4
Steve Bachmeier <sbachmei@uw.edu>
2026-04-16 09:55:23 -0600

    Revert "Updating hemoglobin release ID to 33 (#135)"
    This reverts commit 8b57f921134945906086fcea7eaecbbfdd6fc1d7.

--- src/vivarium_gates_nutrition_optimization/data/extra_gbd.py
+++ src/vivarium_gates_nutrition_optimization/data/extra_gbd.py
@@ -133,6 +133,6 @@ def get_hemoglobin_exposure_data(key: str, location: str):
         location_id=location_id,
         year_id=2023,
         sex_id=gbd_constants.SEX.FEMALE,
-        release_id=33,
+        release_id=gbd_constants.RELEASE_IDS.GBD_2023,
     )
     return data
