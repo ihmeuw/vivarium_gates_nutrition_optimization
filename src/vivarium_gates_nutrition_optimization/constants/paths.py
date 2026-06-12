6cd661fd26136fb60004130a487f485a90226273
Sylvia Lutze <lutzes@long-slurm-sarchive-p0115.cluster.ihme.washington.edu>
2026-06-12 08:13:38 -0700

    Adding new hgb data file and updating path

--- src/vivarium_gates_nutrition_optimization/constants/paths.py
+++ src/vivarium_gates_nutrition_optimization/constants/paths.py
@@ -14,7 +14,7 @@ CSV_RAW_DATA_ROOT = BASE_DIR / "data" / "raw_data"
 
 # Proportion of pregnant women with hemoglobin less than 70 g/L
 PREGNANT_PROPORTION_WITH_HEMOGLOBIN_BELOW_70_CSV = (
-    CSV_RAW_DATA_ROOT / "pregnant_proportion_with_hgb_below_70_age_specific.csv"
+    CSV_RAW_DATA_ROOT / "pregnant_proportion_with_hgb_below_70_age_specific_2023.csv"
 )
 
 HEMOGLOBIN_MATERNAL_DISORDERS_PAF_CSV = (
